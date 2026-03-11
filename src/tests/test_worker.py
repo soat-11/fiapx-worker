faf6311d8c3277af88d920741fa567d10e54964e
Eduardo Santo <eesanto.ciet@rd.com.br>
2026-03-11 19:40:51 -0300

    fix: atualiza moto para mock_aws

--- src/tests/test_worker.py
+++ src/tests/test_worker.py
@@ -3,9 +3,8 @@ import json
 import boto3
 import pytest
 from pathlib import Path
-from moto import mock_s3, mock_sqs
+from moto import mock_aws 
 
-# Configura ambiente antes do import
 AWS_REGION = 'us-east-1'
 os.environ['SQS_QUEUE_URL'] = f'https://sqs.{AWS_REGION}.amazonaws.com/123/processing-queue'
 os.environ['RESULT_QUEUE_URL'] = f'https://sqs.{AWS_REGION}.amazonaws.com/123/result-queue'
@@ -14,11 +13,10 @@ os.environ['AWS_DEFAULT_REGION'] = AWS_REGION
 
 from app.worker import VideoWorker
 
+@mock_aws 
 class TestVideoWorker:
     AWS_REGION = AWS_REGION
 
-    @mock_s3
-    @mock_sqs
     def setup_method(self, method):
         self.s3 = boto3.client("s3", region_name=self.AWS_REGION)
         self.sqs = boto3.client("sqs", region_name=self.AWS_REGION)
@@ -30,7 +28,6 @@ class TestVideoWorker:
         self.worker = VideoWorker()
 
     def test_tmp_directory_usage(self):
-        # Valida a lógica de negócio do seu worker.py
         video_id = "test_123"
         work_dir = Path(f"/tmp/{video_id}")
         assert str(work_dir).startswith("/tmp")
